a79199d397787e8e57152d94d27b921bb76077d3
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-01-30 14:08:10 +0000

    Initial Commit (ES Lint Changes + Setup)

--- Article/Article.js
+++ Article/Article.js
@@ -7,9 +7,9 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+
   }
 
   expandArticle() {
@@ -18,9 +18,9 @@ class Article {
   }
 }
 
-/* START HERE: 
+/* START HERE:
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable.
 
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
